4d7b089fb783b2dee6b2590f00dc0a07dca9e94f
hraboviyvadim <hraboviyvadim@gmail.com>
2013-11-25 16:00:44 +0200

    author page added

--- js/common.js
+++ js/common.js
@@ -1,5 +1,26 @@
 $(document).ready(function() {
 
+	// select
+
+	$(".select__dropdown").hide();
+
+	$(document).click( function(event){
+    	$(".select__dropdown").slideUp('fast');
+    });
+
+	$(".select").click(function(){
+      	$(this).parent().find(".select__dropdown").slideToggle('fast');
+      	event.stopPropagation();
+    });
+
+	$(".select__dropdown li").on("click", function(event){
+		var select = $(this).text();
+		var data_attr = $(this).attr('data-attr');
+		$(this).parents('.l-select').find('.select__value').text(select);
+		$(this).parents('.l-select').find('input').val(data_attr);
+		$(this).parent().slideUp('fast');
+	});
+
 
 
 });
