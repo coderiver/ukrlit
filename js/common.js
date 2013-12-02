d413bf1f84651946da8c9e6a150d9b6b925f192f
hraboviyvadim <hraboviyvadim@gmail.com>
2013-12-02 10:22:07 +0200

    fix #9

--- js/common.js
+++ js/common.js
@@ -54,6 +54,6 @@ $(document).ready(function() {
 	$(".scroll-top__inner").click(function(){
         $("html, body").animate({ scrollTop: 0 }, 400);
         return false;
-        });
+    });
 
 });
